3adaba1ffece0a3f4349997d302c2536c49a2a50
Mian <mianchd@gmail.com>
2019-09-19 21:21:14 -0400

    changing to hello

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
-print("Hellloo World!")
-print(True)
+"""
+helloworld.py
 
-print([1, 2, 4])
-print("best week ever")
+A program to say hello
+"""
+
+if __name__ == '__main__':
+    print('Hello, Git world!')
